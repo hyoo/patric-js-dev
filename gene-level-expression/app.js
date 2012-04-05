f1225a31617569096a0339fb1e28debdb0b73d25
Harry Yoo <hyun@vbi.vt.edu>
2012-04-05 12:24:40 -0400

    setting parameters to stores when the application is launched

--- gene-level-expression/app.js
+++ gene-level-expression/app.js
@@ -1,16 +1,10 @@
-
-
 Ext.onReady(function() {
-
 	//console.log('Ext ready!');
-
 	// create the app
 	Ext.application({
 		
 		name: 'CoordView',
-		
 		appFolder: 'app',
-		
 		models: [
 			'Genome',
 			'Feature',
@@ -21,7 +15,6 @@ Ext.onReady(function() {
 			'IsoSource',
 			'Pathovar'
 		],
-	
 		controllers: [
 			'ViewController'
 		],
@@ -38,18 +31,23 @@ Ext.onReady(function() {
 					});
 				}
 			});
-  		*/
-  		
-  		CoordView.filterCall=null;
-  		
-  		CoordView.filter=function(field, value, re){
-  			//console.log(newValue);
+			*/
+			var featureId = Ext.getDom('featureId').value;
+			//console.log("featureId="+featureId);
+			Ext.getStore('Features').getProxy().extraParams.featureId = featureId;
+			Ext.getStore('Accessions').getProxy().extraParams.featureId = featureId;
+			Ext.getStore('Platforms').getProxy().extraParams.featureId = featureId;
+		
+			CoordView.filterCall=null;
+		
+			CoordView.filter=function(field, value, re){
+			//console.log(newValue);
 				
 				var store=Ext.getStore('Features');
 				//store.suspendEvents();
 				store.clearFilter();
 				//store.resumeEvents();
-
+				
 				var countStr='';
 				var count=0;
 				if (value=='' || value==null || value=='Filter') {
@@ -68,25 +66,24 @@ Ext.onReady(function() {
 					}
 					Ext.getCmp('filterReport').setText(countStr+' '+valueStr);
 				}
-  		};
-  		
+			};
+			
 			/*
 			 * Creates the main UI.
 			*/
-			Ext.create('Ext.window.Window', {
-				title: 'I Can Has E coli Plz?',
+			//Ext.create('Ext.window.Window', {
+			Ext.create('Ext.panel.Panel', {
+				renderTo: 'expression_panel',
+				//title: 'I Can Has E coli Plz?',
 				id: 'p-genomelist-ui',
 				layout: 'border',
-				
 				minHeight: 600,
 				minWidth: 700,
 				width: 1000,
 				height: 700,
-
 				maximizable: true,
 				minimizable: false,
 				closable: false,
-				
 				items: [
 					{
 						region: 'center',
@@ -100,7 +97,6 @@ Ext.onReady(function() {
 								region: 'south',
 								height: 250,
 								layout: 'fit',
-								
 								items: [
 									{
 										xtype: 'tabpanel',
@@ -128,7 +124,6 @@ Ext.onReady(function() {
 							type: 'vbox',
 							align: 'center'
 						},
-						
 						items: [
 							{
 								xtype: 'tabpanel',
@@ -177,7 +172,6 @@ Ext.onReady(function() {
 						]
 					}
 				],
-				
 				dockedItems: [
 					{
 						xtype: 'toolbar',
@@ -194,7 +188,6 @@ Ext.onReady(function() {
 								value: '',
 								emptyText: 'Filter',
 								scope: this,
-								
 								listeners: {
 									
 									specialkey: function(field, e){
@@ -207,7 +200,6 @@ Ext.onReady(function() {
 											CoordView.filter('exp_name', val, true);
 										}
 									}/*,
-									
 									change: function(field, newValue, oldValue, options){
 										if (newValue!=oldValue) {
 											clearTimeout(CoordView.filterCall);
@@ -220,7 +212,6 @@ Ext.onReady(function() {
 									}
 									*/
 								}
-								
 							},
 							'->',
 							{
@@ -231,13 +222,7 @@ Ext.onReady(function() {
 						]
 					}
 				]
-	
 			}).show();
-			
 		}
-
 	});	
 });
-
-
-

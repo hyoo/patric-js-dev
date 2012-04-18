1e430ef80a09e7d1063eeb2487b4b1e093d37c6f
Harry Yoo <hyun@vbi.vt.edu>
2012-04-18 12:32:22 -0400

    changed id (rownum) to exp_geneid

--- gene-level-expression/app/model/Feature.js
+++ gene-level-expression/app/model/Feature.js
@@ -7,10 +7,10 @@
  */
 Ext.define('CoordView.model.Feature', {
 	extend: 'Ext.data.Model',
-	idProperty: 'rownum',
+	idProperty: 'exp_geneid',
 	fields: [
 		'exp_accession', 'exp_samples', 'exp_platform', 'exp_name', 'exp_locustag',
 		{name:'exp_pavg', type:'float', useNull:false}, {name:'exp_pratio', type:'float', useNull:false}, 'patric_locus_tag', 'figfam_id', 
-		{name:'rownum', type:'int'}
+		{name:'exp_geneid', type:'int'}
 	]
 });

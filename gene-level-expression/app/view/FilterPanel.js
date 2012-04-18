9e4c57098052be57d56f7c9c093983464cbdc112
Harry Yoo <hyun@vbi.vt.edu>
2012-04-18 12:38:45 -0400

    added "filter" and "reset" filter actions

--- gene-level-expression/app/view/FilterPanel.js
+++ gene-level-expression/app/view/FilterPanel.js
@@ -14,49 +14,85 @@ Ext.define('CoordView.view.FilterPanel', {
 	items: [
 		{
 			xtype: 'textfield',
-			name: 'filterField',
-			id: 'filterTextField',
-			itemId: 'filterText',
-			width: 125,
+			itemId: 'keyword',
+			width: 200,
 			hideLabel: true,
 			allowBlank: true,
 			value: '',
-			emptyText: 'Filter',
-			scope: this,
+			emptyText: 'keyword',
+			scope: this /*,
 			listeners: {
 				specialkey: function(field, e){
 					// e.HOME, e.END, e.PAGE_UP, e.PAGE_DOWN,
 					// e.TAB, e.ESC, arrow keys: e.LEFT, e.RIGHT, e.UP, e.DOWN
 					if (e.getKey() == e.ENTER) {
-						clearTimeout(CoordView.filterCall);
-						CoordView.filterCall=null;
+						//clearTimeout(CoordView.filterCall);
+						//CoordView.filterCall=null;
 						var val=field.getValue();
 						CoordView.filter('exp_name', val, true);
 					}
 				}
+			}*/
+		},
+		{
+			xtype: 'tbspacer',
+			width: 20
+		},
+		{
+			xtype: 'combobox',
+			itemId: 'threshold',
+			fieldLabel: 'Select fold change',
+			queryMode: 'local',
+			displayField: 'name',
+			labelWidth: 105,
+			value: 0,
+			store: Ext.create('Ext.data.Store', {
+				fields: ['name'],
+				data: [{name:"0"}, {name:"0.5"}, {name:"1"}, {name:"1.5"}, {name:"2"}, {name:"2.5"}, {name:"3"}, {name:">3"}]
+			}),			
+			editable: false
+		},
+		{
+			xtype: 'tbspacer',
+			width: 20
+		},
+		{
+			xtype: 'button',
+			text: 'Filter',
+			itemId: 'filter',
+			handler: function() {
+				var param = new Object();
+				param.keyword = this.ownerCt.getComponent("keyword").value;
+				param.threshold = this.ownerCt.getComponent("threshold").value;
+				
+				CoordView.param = Ext.Object.merge(CoordView.param, param);
+				
+				Ext.getStore('Features').filterOnFly(CoordView.param);
+				Ext.getStore('ExpressionRatios').load();
+				Ext.getStore('Accessions').load();
 			}
 		},
 		{
-			xtype: 'numberfield',
-			name: 'threshold',
-			id: 'filter_threshold',
-			fieldLabel: 'fold change',
-			step: 0.5,
-			value: 0,
-			minValue: 0
-		}, { /*
-			xtype: 'textfield',
-			name: 'filter_keyword',
-			id: 'filter_keyword',
-			emptyText: 'enter keyword'
-		}, {*/
+			xtype: 'tbspacer',
+			width: 20
+		},
+		{
 			xtype: 'button',
-			text: 'Filter',
+			text: 'Reset Filter',
 			handler: function() {
-				//console.log("filter!!")
-				//console.log(this);
-				thrhd = Ext.getCmp("filter_threshold").value;
-				Ext.getStore('Features').filterByRatio(thrhd);
+				// reset interface
+				this.ownerCt.getComponent("keyword").setValue('');
+				this.ownerCt.getComponent("threshold").setValue(0);
+				
+				// reset CoordView.param
+				var param = new Object({keyword:'', threshold:''});
+				CoordView.param = Ext.Object.merge(CoordView.param, param);
+				
+				// reload
+				Ext.getStore('Features').clearFilter();
+				Ext.getStore('Features').updateRecordCount();
+				Ext.getStore('ExpressionRatios').load();
+				Ext.getStore('Accessions').load();
 			}
 		}
 	]

2a22b1bf093ccdec36bab3fd37b71117a14d92a1
Harry Yoo <hyun@vbi.vt.edu>
2012-05-08 10:32:35 -0400

    add tooltip

--- gene-level-expression/app/view/FeatureGrid.js
+++ gene-level-expression/app/view/FeatureGrid.js
@@ -5,6 +5,11 @@
  *
  * This class implements a grid of players.
  */
+function BasicRenderer(value, metadata, record, rowIndex, colIndex, store){
+	metadata.tdAttr = 'data-qtip="'+value+'" data-qclass="x-tip"';
+	return value;
+};
+
 Ext.define('CoordView.view.FeatureGrid', {
 	extend: 'Ext.grid.Panel',
 	alias: 'widget.featuregrid',
@@ -15,10 +20,10 @@ Ext.define('CoordView.view.FeatureGrid', {
 		{dataIndex: 'exp_platform',		header: 'Platform',		flex: 1, hidden: true},
 		{dataIndex: 'exp_samples',		header: 'Samples',		flex: 1, hidden: true},
 		{dataIndex: 'exp_locustag',		header: 'Locus Tag',	flex: 1, hidden: true},
-		{dataIndex: 'exp_name',			header: 'Exp Name',		flex: 3},
-		{dataIndex: 'exp_strain',		header: 'Strain',		flex: 1},
-		{dataIndex: 'exp_mutant',		header: 'Mutant',		flex: 1},
-		{dataIndex: 'exp_condition',	header: 'Condition',	flex: 1},
+		{dataIndex: 'exp_name',			header: 'Exp Name',		flex: 4,	renderer:BasicRenderer},
+		{dataIndex: 'exp_strain',		header: 'Strain',		flex: 1,	renderer:BasicRenderer},
+		{dataIndex: 'exp_mutant',		header: 'Mutant',		flex: 1,	renderer:BasicRenderer},
+		{dataIndex: 'exp_condition',	header: 'Condition',	flex: 2,	renderer:BasicRenderer},
 		{dataIndex: 'exp_pavg',			header: 'Avg Intensity',flex: 1},
 		{dataIndex: 'exp_pratio',		header: 'Ratio',		flex: 1},
 		{dataIndex: 'exp_zscore',		header: 'Z Score',		flex: 1}

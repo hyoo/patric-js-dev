d8c26bef426922603df6176c4acde6a0bd1115bf
Harry Yoo <hyun@vbi.vt.edu>
2012-05-08 10:28:23 -0400

    pick up the filter param to add to app on init

--- gene-level-expression/app.js
+++ gene-level-expression/app.js
@@ -6,9 +6,15 @@ Ext.application({
 	launch: function() {
 		var param = new Object();
 		param.featureId = Ext.getDom('featureId').value;
-		param.sampleId = Ext.getDom('sampleId').value;
-		param.filter = Ext.getDom('filter').value;
-		param.cutoff = Ext.getDom('cutoff').value;
+		param.sampleId = Ext.getDom("sampleId")?Ext.getDom('sampleId').value:"";
+		param.filter = Ext.getDom("filter")?Ext.getDom('filter').value:"";
+		param.cutoff = Ext.getDom("cutoff")?Ext.getDom('cutoff').value:"";
+		if (param.filter != "" && param.cutoff != "") {
+			fp = Ext.getCmp("p-filterpanel");
+			//console.log(fp);
+			fp.getComponent("filter").setValue(param.filter);
+			fp.getComponent("cutoff").setValue(param.cutoff);
+		}
 		CoordView.param = param;
 	}
 });	

6a74a32d26b23f916ddc9e19e08be948ceecd9af
Harry Yoo <hyun@vbi.vt.edu>
2012-09-04 16:28:13 -0400

    correct file name

--- transcriptomics-upload/build.sh
+++ transcriptomics-upload/build.sh
@@ -1,9 +1,9 @@
-rm TrasnscriptomicsUpload.debug.js
-cat app/model/* >> TrasnscriptomicsUpload.debug.js
-cat app/store/* >> TrasnscriptomicsUpload.debug.js
-cat app/view/* >> TrasnscriptomicsUpload.debug.js
-cat app/controller/*.js >> TrasnscriptomicsUpload.debug.js
-cat app.js >> TrasnscriptomicsUpload.debug.js
-java -jar ../yuicompressor-2.4.7.jar TrasnscriptomicsUpload.debug.js > TrasnscriptomicsUpload.js
-#cp TrasnscriptomicsUpload.debug.js ../../js/vbi/
-#cp TrasnscriptomicsUpload.js ../../js/vbi/
+rm TranscriptomicsUpload.debug.js
+cat app/model/* >> TranscriptomicsUpload.debug.js
+cat app/store/* >> TranscriptomicsUpload.debug.js
+cat app/view/* >> TranscriptomicsUpload.debug.js
+cat app/controller/*.js >> TranscriptomicsUpload.debug.js
+cat app.js >> TranscriptomicsUpload.debug.js
+java -jar ../yuicompressor-2.4.7.jar TranscriptomicsUpload.debug.js > TranscriptomicsUpload.js
+cp TranscriptomicsUpload.debug.js ../../js/vbi/
+cp TranscriptomicsUpload.js ../../js/vbi/

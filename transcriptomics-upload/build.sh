d709bd323ec9591146fefef727d5cd03271c4427
Harry Yoo <hyun@vbi.vt.edu>
2012-09-06 16:39:08 -0400

    added build script and test page

--- transcriptomics-upload/build.sh
+++ transcriptomics-upload/build.sh
@@ -1,9 +1,9 @@
 rm TranscriptomicsUpload.debug.js
 cat app/model/* >> TranscriptomicsUpload.debug.js
 cat app/store/* >> TranscriptomicsUpload.debug.js
-cat app/view/* >> TranscriptomicsUpload.debug.js
+cat app/view/*.js >> TranscriptomicsUpload.debug.js
 cat app/controller/*.js >> TranscriptomicsUpload.debug.js
 cat app.js >> TranscriptomicsUpload.debug.js
 java -jar ../yuicompressor-2.4.7.jar TranscriptomicsUpload.debug.js > TranscriptomicsUpload.js
-cp TranscriptomicsUpload.debug.js ../../js/vbi/
-cp TranscriptomicsUpload.js ../../js/vbi/
+#cp TranscriptomicsUpload.debug.js ../../js/vbi/TranscriptomicsUpload.debug.js
+#cp TranscriptomicsUpload.js ../../js/vbi/TranscriptomicsUpload.js

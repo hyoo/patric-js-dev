7a36d67f10ce0d0d5ea07f9a831c617c4d0b9193
Harry Yoo <hyun@vbi.vt.edu>
2012-10-23 11:30:29 -0400

    added AddToWorkspace.js into this project

--- workspace/build.sh
+++ workspace/build.sh
@@ -6,5 +6,8 @@ cat app/view/*.js >> Workspace.js
 cat app/controller/*.js >> Workspace.js
 cat app.js >> Workspace.js
 java -jar ../yuicompressor-2.4.7.jar Workspace.js > Workspace.min.js
+java -jar ../yuicompressor-2.4.7.jar AddToWorkspace.js > AddToWorkspace.min.js
 cp Workspace.js ../../js/vbi/Workspace.js
 cp Workspace.min.js ../../js/vbi/Workspace.min.js
+cp AddToWorkspace.js ../../js/vbi/AddToWorkspace.js
+cp AddToWorkspace.min.js ../../js/vbi/AddToWorkspace.min.js

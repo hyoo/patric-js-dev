08f1e1b872821a523ad9c77f2644eff5d852ec69
Harry Yoo <hyun@vbi.vt.edu>
2012-12-19 16:48:42 -0500

    modified according to the changes in namespace and filename convention

--- genome-selector/build.sh
+++ genome-selector/build.sh
@@ -1,3 +1,7 @@
+rm GenomeSelector.js
+cat app/model/* >> GenomeSelector.js
+cat app/store/* >> GenomeSelector.js
+cat app/view/* >> GenomeSelector.js
 java -jar ../yuicompressor-2.4.7.jar GenomeSelector.js > GenomeSelector.min.js
-cp GenomeSelector.js ../../js/vbi/GenomeSelector.js
-cp GenomeSelector.min.js ../../js/vbi/GenomeSelector.min.js
+#cp GenomeSelector.js ../../js/vbi/GenomeSelector.js
+#cp GenomeSelector.min.js ../../js/vbi/GenomeSelector.min.js

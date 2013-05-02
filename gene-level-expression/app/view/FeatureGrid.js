4f179e4b98aa1347b5df563c4a2593588c9c4a44
Harry Yoo <hyun@vbi.vt.edu>
2013-05-02 16:57:01 -0400

    added link to gene list

--- gene-level-expression/app/view/FeatureGrid.js
+++ gene-level-expression/app/view/FeatureGrid.js
@@ -19,7 +19,13 @@ Ext.define('VBI.GeneExpression.view.FeatureGrid', {
 		{dataIndex: 'exp_platform',		header: 'Platform',		flex: 1, hidden: true},
 		{dataIndex: 'exp_samples',		header: 'Samples',		flex: 1, hidden: true},
 		{dataIndex: 'exp_locustag',		header: 'Locus Tag',	flex: 1, hidden: true},
-		{dataIndex: 'exp_name',			header: 'Title',		flex: 4, renderer:BasicRenderer},
+		{dataIndex: 'exp_name',			header: 'Title',		flex: 4, 
+			renderer: function(value, metadata, record, rowIndex, colIndex, store) {
+				var log_ratio = VBI.GeneExpression.param.log_ratio || 0,
+					zscore = VBI.GeneExpression.param.zscore || 0;
+				return Ext.String.format('<a href="TranscriptomicsGene?cType=taxon&cId={1}&dm=result&expId={2}&sampleId=&colId=&log_ratio={3}&zscore={4}" target="_blank">{0}</a>', value, 2, record.data.exp_id,  log_ratio, zscore);
+			}
+		},
 		{dataIndex: 'pmid',				header: 'PubMed',		flex: 1, 
 			renderer: function(value, metadata, record, rowIndex, colIndex, store) {
 				if (value != undefined && value != "") {
